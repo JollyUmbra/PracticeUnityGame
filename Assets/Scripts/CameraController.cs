89d8c8fdb2cdc582103675b1d1768996a8a03e2b
JollyUmbra <mwooddesigns@gmail.com>
2015-06-01 21:31:44 -0400

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+
+	private bool movingLeft = false;
+	private bool movingRight = false;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (target && target.position.x > transform.position.x && movingRight || target && target.position.x < transform.position.x && movingLeft)
+		{
+			Vector3 delta = new Vector3();
+			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+			if(target.position.y <= 0.1)
+			{
+				delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.1f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			}
+			if(target.position.y > 0.1)
+			{
+				delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.4f, point.z));
+			}
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
+		if(Input.GetAxis("Horizontal") > 0)
+		{
+			movingLeft = false;
+			movingRight = true;
+		}
+		if(Input.GetAxis("Horizontal") < 0)
+		{
+			movingLeft = true;
+			movingRight = false;
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float speed = 1f;
+	public float jumpForce = 100f;
+	Rigidbody2D player;
+
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	bool grounded = false;
+
+	void Start()
+	{
+		player = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update()
+	{
+		float jump = Input.GetAxis("Jump");
+
+		if(grounded && jump > 0)
+		{
+			player.AddForce(new Vector2(0, jumpForce));
+			grounded = false;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+		float move = Input.GetAxis("Horizontal");
+		player.velocity = new Vector2(move*speed, player.velocity.y);
+	}
+}
